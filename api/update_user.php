83f89fe893eb491cc4713754ae5d61528d263b90
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-04 11:01:16 -0300

    criação do arquivo php update-user

---
+++ api/update_user.php
@@ -0,0 +1,63 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+
+include_once 'config/database.php';
+include_once 'objects/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = new USer($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$jwt = isset($data->jwt) ? $data->jwt : "";
+
+if ($jwt) {
+    # code...
+    try {
+        //code...
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+
+        $user->firstname = $data->firstname;
+        $user->lastname = $data->lastname;
+        $user->email = $data->email;
+        $user->password = $data->password;
+        $user->id = $decoded->data_id;
+
+        if ($user->update()) {
+            # code...
+        } else {
+            # code...
+            http_response_code(401);
+
+            echo json_encode(array("message" => "Não foi possível atualizar o usuário"));
+        }
+        
+
+
+
+
+
+    } catch (Exception $e) {
+        //Exception
+        http_response_code(401);
+
+        echo json_encode(array(
+            "message"=> "Acesso negado.",
+            "error" => $e->getMessage()
+        ));
+    }
+}
+?>
